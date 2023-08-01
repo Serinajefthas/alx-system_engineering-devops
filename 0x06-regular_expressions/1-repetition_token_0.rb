916de41406244eec7f74723cda9494349c79f4fc
Serinajefthas <serina.jefthas@gmail.com>
2023-08-01 13:21:28 -0700

    Add tasks 2-6

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Script that matches all text inputted
+
+puts ARGV[0].scan(/hbt{2,5}n/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Script to match repeating text
+
+puts ARGV[0].scan(/hb?t?n/).join
