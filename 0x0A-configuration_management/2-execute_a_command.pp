09f80a53253557c8adfce605abea3369da8d8e46
Serinajefthas <serina.jefthas@gmail.com>
2023-09-22 09:01:48 -0700

    Remove additional argument

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,6 +2,4 @@
 exec { 'pkill':
   command     => 'pkill killmenow',
   provider    => 'shell',
-  onlyif      => 'pgrep killmenow', #check that process killmenow exists
-  path        => ['/bin', '/usr/bin'], #path puppet agent find pkill, pgrep
 }
