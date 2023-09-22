48d812e93c7873a95ad48879af686ce95b6ff257
Serinajefthas <serina.jefthas@gmail.com>
2023-09-22 09:06:58 -0700

    Update syntax errors

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 #kills process 'killmenow' using pkill cmd
+
 exec { 'pkill':
-  command     => 'pkill killmenow',
-  provider    => 'shell',
+  command  => 'pkill killmenow',
+  provider => 'shell',
+  path     => '/usr/bin/'
 }
