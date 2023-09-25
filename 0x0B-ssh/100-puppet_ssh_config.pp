b83f8a1c7372ed7fa3d188e2758abc054248412e
Serinajefthas <serina.jefthas@gmail.com>
2023-09-24 21:09:48 -0700

    Updates to allow acess to my server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Connect to server using puppet format
+# client config file
 exec { 'echo "PasswordAuthentication no
 	IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
