024add602facfd0e0915e61174293cb236b11d13
Serinajefthas <serina.jefthas@gmail.com>
2023-10-02 13:58:59 -0700

    Fix syntax issues

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,7 +2,7 @@
 exec { 'cmd':
   command => 'apt-get -y update;
   apt-get -y install nginx;
-  sudo sed -i '/listen 80 default_server;/a add_header X-Served_by $HOSTNAME;' /etc/nginx/sites_available/default;
-  service nginx start
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served_by $HOSTNAME;" /etc/nginx/sites_available/default;
+  service nginx start',
   provider => shell,
 }
