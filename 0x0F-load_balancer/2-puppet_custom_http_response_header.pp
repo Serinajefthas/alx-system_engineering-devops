f70ee5b83d23c422b8ff871ca1b9d483cb23c80f
Serinajefthas <serina.jefthas@gmail.com>
2023-10-02 14:02:23 -0700

    Task 2 errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 # Script creates custom http header response but this time w Puppet
-exec { 'cmd':
-  command => 'apt-get -y update;
+exec { 'command':
+  command  => 'apt-get -y update;
   apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served_by $HOSTNAME;" /etc/nginx/sites_available/default;
-  service nginx start',
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
 }
