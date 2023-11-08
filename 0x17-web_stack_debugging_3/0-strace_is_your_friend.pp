5a1ac23881feefb73def70c96098a2f17a50a394
Serinajefthas <serina.jefthas@gmail.com>
2023-11-08 13:45:59 -0800

    fix task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
-#Script to fix apache returning 500 internal server error
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  require => Package['nginx'], # Ensure Nginx package is installed before managing the file
-}
-exec { 'replace_sites_enabled':
-  command     => 'sed -i s/sites-enabled/sites-available/g /etc/nginx/nginx.conf',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/nginx.conf'],
-  notify      => Service['nginx'],
-}
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['replace_sites_enabled'],
+#Script to fix apache returning 500 internal server error due to wordpress file extension error
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
