92d7afa39a029f2fe52cb70b52a714218b82bf7c
Serinajefthas <serina.jefthas@gmail.com>
2023-11-08 13:36:13 -0800

    Add readme and task 0

---
+++ 0x17-web_stack_debugging_3/2-strace_is_your_friend.pp
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#Script to fix apache returning 500 internal server error
+#sudo sed -i 's/sites-enabled/sites-available/g' "/etc/nginx/nginx.conf";
+#sudo service nginx restart
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  require => Package['nginx'], # Ensure Nginx package is installed before managing the file
+}
+exec { 'replace_sites_enabled':
+  command     => 'sed -i s/sites-enabled/sites-available/g /etc/nginx/nginx.conf',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/nginx.conf'],
+  notify      => Service['nginx'],
+}
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['replace_sites_enabled'],
+}
