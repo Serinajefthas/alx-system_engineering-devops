f88454277428afd3b79248796b45df7b27a25e6b
Serinajefthas <serina.jefthas@gmail.com>
2023-11-09 14:49:21 -0800

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#Script that utilises reddit API to return number of subscribers for given subreddit
+import requests
+
+def number_of_subscribers(subreddit):
+    """Returns number of subscribers for given subreddit else 0"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'MysubredditSubsRetreivalScript/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
+
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Project on using APIs in real world examples for practice
