9285906cda7d65d3b8ede9b9e15a47b8a0dd8000
Serinajefthas <serina.jefthas@gmail.com>
2023-11-16 23:07:59 +0000

    fix task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 #Script updates open file limit of nginx server to allow more requests to be managedi
 
-file { '/etc/default/nginx':
-    content => "ulimit -n 4096",
+exec { 'update-ulimit':
+    path    => '/usr/local/bin/:/bin/'
+    command => "ulimit -n 4096",
 }
 exec { 'restart_nginx':
     command => 'service nginx restart',
